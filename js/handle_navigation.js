c6b70546c6486aad90d1ef66b9b45b48617921da
lucacimo <luca.cimonetti@gmail.com>
2018-03-03 20:29:44 +0100

    added media queries for responsive design

---
+++ js/handle_navigation.js
@@ -0,0 +1,64 @@
+    // When the user scrolls the page, execute myFunction 
+    window.onscroll = function() {myFunction()};
+               
+    // Get the navbar
+    var navbar = document.getElementById("navbar");
+    var about = document.getElementById("about");
+    var logo = document.getElementById("logo");
+    
+    // Get the offset position of the navbar
+    var sticky = spacing.offsetTop;
+
+    //Get hamburger icon 
+    var hamburger = document.getElementById("hamburger");
+    var open = false;
+    
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+
+         var list = document.getElementById("navigation__list");
+         if (window.pageYOffset >= sticky) {
+             navbar.classList.add("sticky");
+             logo.classList.add("logosticky");
+             document.getElementById("navbar").style.backgroundColor="#101d2c";
+      } 
+      else if(window.pageYOffset <= sticky && !open){
+        navbar.classList.remove("sticky");
+        logo.classList.remove("logosticky");
+        document.getElementById("navbar").style.backgroundColor="transparent";
+                           
+      }
+    }
+    
+    hamburger.addEventListener("click", function(){
+
+        if (open) {
+            console.log("it is open and it is going to close");
+            document.getElementById("navigation__list").style.display = "none";
+            document.getElementById("social__list").style.display="none";
+            document.getElementById("nav-icon").classList.add("fas", "fa-bars");
+            document.getElementById("nav-icon").classList.remove("far", "fa-window-close");
+            open = false;
+        }
+        
+        else{
+            console.log("it is close and is going to open");
+            document.getElementById("navigation__list").style.display = "flex";
+            document.getElementById("social__list").style.display = "flex";
+            document.getElementById("navbar").style.backgroundColor="#101d2c";
+            document.getElementById("nav-icon").classList.add("far", "fa-window-close");
+            document.getElementById("nav-icon").classList.remove("fas", "fa-bars");
+            open = true;
+        }
+
+        if (!open && pageYOffset <= sticky) {
+            document.getElementById("navbar").style.backgroundColor = "transparent";
+        }
+
+        if (open && pageYOffset <= sticky) {
+            document.getElementById("navbar").style.backgroundColor = "#101d2c";
+        }
+
+
+
+    });        
